6772f9b9b93edfbd36d6f80a79156aed93fffb2f
Scott <scottrfessler@gmail.com>
2017-06-26 23:02:21 -0400

    Set user permissions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
@@ -64,11 +66,17 @@ class ListingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
-      @listing = Listing.find(params[:id])
+      @listing = Listing.find params[:id]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
+    end
+
+    def check_user
+      if current_user != @listing.user
+      redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+      end
     end
 end
