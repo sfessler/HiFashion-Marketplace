9e98b3d7fd1b364fd236114317097cc93f99aceb
Scott <scottrfessler@gmail.com>
2017-06-27 10:48:59 -0400

    Add Seller Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'seller' => "listings#seller"
+
   root 'listings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
