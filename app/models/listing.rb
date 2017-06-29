0a8c985579ac2fe0d068235d916020ffbd72a309
Scott <scottrfessler@gmail.com>
2017-06-29 13:07:25 -0400

    removing default pic

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
 	        has_attached_file :image, styles: { medium: "200x>", thumb: "100x>" }
 	else
 	        has_attached_file :image, styles: { medium: "200x>", thumb: "100x>" },
-					  :default_url=> "images.jpeg",
+					  #:default_url=> "images.jpeg",
 					  :path => ":style/:id_:filename",
     				  :storage => :s3,
     			   	  :s3_region => "us-east-2",
